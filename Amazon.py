f654d20d2c352321dbade8e4976f34d9bf3a1a0e
Kenji Liu <kenjihell9563723@gmail.com>
2019-02-09 16:07:42 +0800

    calc avg len reviews

--- Amazon.py
+++ Amazon.py
@@ -5,10 +5,20 @@ with open('reviews.txt', 'r') as f:
 		data.append(line)
 		count += 1 #count = count + 1
 		if count % 1000 == 0:    # %是求餘數
-		print(len(data)) #每讀一行列出來進度
-print(len(data)) #讀取留言檔
+		    print(len(data)) #每讀一行列出來進度
+print('檔案讀取完了,總共有', len(data), '筆資料') #讀取留言檔
 
-print(data)  #印出data清單
-print(data[0]) #印出第一筆留言
-print('-------------------------------')
-print(data[1]) #印出第二筆留言
+#print(data)  #印出data清單
+#print(data[0]) #印出第一筆留言
+#print('-------------------------------')
+#print(data[1]) #印出第二筆留言
+
+
+#算出留言平均長度
+
+sum_len = 0
+for d in data:   #每一筆資料都叫做d 
+    sum_len = sum_len + len(d) 
+    print(sum_len)
+
+print('留言的平均長度為', sum_len/len(data))
